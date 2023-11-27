a043fd0d7b70addf54be12a8280328fefd682151
Lavisha Kapoor <kapoorlavisha0@gmail.com>
2023-11-27 16:45:50 +0530

    changes made

--- PrintPalindrome.java
+++ PrintPalindrome.java
@@ -1,22 +1,21 @@
 package InterviewPrograms;
 
 public class PrintPalindrome {
-	
-public static void main(String args[]) {
-	
-	String s1 = "maliuytm";
-	String revStr="";
-	
-	for (int i=s1.length()-1;i>=0;i--) {		
-		revStr = revStr + s1.charAt(i);				
+
+	public static void main(String args[]) {
+
+		String s1 = "mimim";
+		String revStr = "";
+
+		for (int i = s1.length() - 1; i >= 0; i--) {
+			revStr = revStr + s1.charAt(i);
+		}
+
+		if (s1.equals(revStr)) {
+			System.out.println("The string " + s1 + " is a Palindrome");
+		} else {
+			System.out.println("The string " + s1 + "is not a Palindrome");
+		}
 	}
-	
-	if(s1.equals(revStr)) {
-		System.out.println("Palindrome");		
-	}
-	else {
-		System.out.println("Not a Palindrome");	
-	}
-}
 
 }
